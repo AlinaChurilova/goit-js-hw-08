3013a83da5b788564736fdfddea1d463a6ce9147
AlinaChurilova <aachcos8@gmail.com>
2022-05-03 18:49:33 +0300

    bugs fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,61 @@
 import throttle from 'lodash.throttle';
+
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('.feedback-form input'),
-    textarea: document.querySelector('.feedback-form textarea'),
-};
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+const form = document.querySelector('.feedback-form');
+
+let fieldData = {};
+
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
 onPopulateMessageOutput();
+
 function onFormSubmit(evt) {
-    evt.preventDefault();
-    console.log(formData)
-    evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-  
-    
+
+    const formData = new FormData(form);
+
+    formData.forEach((value, name) => console.log(value, name));
+
+
+    const formElements = evt.currentTarget.elements;
+    const email = formElements.email.value;
+    const message = formElements.message.value;
+
+    if (email.length && message.length) {
+        evt.preventDefault();
+        console.log(fieldData);
+        evt.currentTarget.reset();
+        localStorage.removeItem(STORAGE_KEY);
+        fieldData = {};
+
+    } else {
+        alert(`Все поля должны быть заполнены!`);
+    };
+        
 };
+
 function onFormInput(evt) {
-    formData[evt.target.name] = evt.target.value;
-    
-  
-    const records = JSON.stringify(formData);
-    
+
+    fieldData[evt.target.name] = evt.target.value;
+            
+    const records = JSON.stringify(fieldData);
+        
     localStorage.setItem(STORAGE_KEY, records);
   
 }
+
 function onPopulateMessageOutput() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    let savedMessage = localStorage.getItem(STORAGE_KEY);
     const parsedData = JSON.parse(savedMessage);
+    
     if (parsedData) {
-      
-        refs.textarea.value = parsedData.message;
-        refs.email.value = parsedData.email;
-    };
+        Object.entries(parsedData).forEach(([name, value]) => {
+            fieldData[name] = value;
+            form.elements[name].value = value;
+        });
+
+    }
 }
+
